13850e47c7c0c7f0a494037befe25ead6886a89d
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-05-10 21:53:12 -0500

    readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRA src/ external imports
+
+- `client/src/App.tsx` uses a typescript module external to the CRA `/src` folder
+- Code builds fine & shows `dt` in console
+- Same approach can be used for the node.js server
--- test/src/App.tsx
+++ test/src/App.tsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { DummyType } from '../../types/dist';
 
+/**
+ * using types shared repository outside of src/ directory
+ * CRA doesn't get made at me
+ */
 const dt: DummyType = {
   x: 10,
   y: 10
 };
 
 function App() {
+  // logging object in console
   console.log(dt);
   return (
     <div className="App">
